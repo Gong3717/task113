97ac11301f2b0a638490415a85b90798022deb20
Your Name <you@example.com>
2017-11-30 11:06:02 +0800

    add files

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(distance,stopminutes) {
+    if(distance<=2){
+        let fee = parseInt(Math.round(6 +stopminutes * 0.25));
+        let result= "The taxi fee is : "+ fee + "(元)\n"
+        return result;
+    }
+    if(distance >2 && distance <8){
+        let fee = parseInt(Math.round(6+(distance-2)*0.8 +stopminutes * 0.25));
+        let result= "The taxi fee is : "+ fee + "(元)\n"
+        return result;
+    }
+    if(distance >=8){
+        let fee = parseInt(Math.round(6+5*0.8 +(distance-8+1)*0.8*1.5+stopminutes * 0.25));
+        let result= "The taxi fee is : "+ fee + "(元)\n"
+        return result;
+    }
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,42 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    describe(" taxiFee",function(){
+        it("return the fee when the distance less than 2km and no stop",function(){
+            let distance = 1.2;
+            let stopminutes = 0;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 6(元)\n");
+        });
+        it("return the fee when the distance less than 2km and stop two minutes",function(){
+            let distance = 1.2;
+            let stopminutes = 2;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 7(元)\n");
+        });
+        it("return the fee when the distance more than 2km and less than 8km and no stop",function(){
+            let distance = 4;
+            let stopminutes = 0;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 8(元)\n");
+        });
+        it("return the fee when the distance more than 2km and less than 8km and stop two minutes",function(){
+            let distance = 4;
+            let stopminutes = 2;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 8(元)\n");
+        });
+        it("return the fee when the distance more than 8km and less than 8km and no stop",function(){
+            let distance = 9;
+            let stopminutes = 0;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 12(元)\n");
+        });
+        it("return the fee when the distance more than 8km and less than 8km and stop two minutes",function(){
+            let distance = 9;
+            let stopminutes = 2;
+            let result = main(distance,stopminutes);
+            expect(result).toEqual("The taxi fee is : 13(元)\n");
+        });
+    });
 });
